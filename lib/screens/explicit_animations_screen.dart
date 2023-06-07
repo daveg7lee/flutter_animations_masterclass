710751f6396d4afd05d305c0ef765fcbfb2afe15
daveg7lee <daveg7lee@gmail.com>
2023-06-07 20:46:44 +0900

    Explicit Widgets

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -15,10 +15,22 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     duration: const Duration(seconds: 2),
   );
 
-  late final Animation<Color?> _color = ColorTween(
-    begin: Colors.amber,
-    end: Colors.red,
+  late final Animation<Decoration> _decoration = DecorationTween(
+    begin: BoxDecoration(
+      color: Colors.amber,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    end: BoxDecoration(
+      color: Colors.red,
+      borderRadius: BorderRadius.circular(120),
+    ),
   ).animate(_animationController);
+
+  late final Animation<double> _rotation =
+      Tween(begin: 0.0, end: 0.5).animate(_animationController);
+
+  late final Animation<double> _scale =
+      Tween(begin: 1.0, end: 1.2).animate(_animationController);
 
   void _play() {
     _animationController.forward();
@@ -33,6 +45,12 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
   }
 
   @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -42,13 +60,22 @@ class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedBuilder(
-                animation: _color,
-                builder: (context, child) => Container(
-                      color: _color.value,
-                      width: 300,
-                      height: 300,
-                    )),
+            ScaleTransition(
+              scale: _scale,
+              child: RotationTransition(
+                turns: _rotation,
+                child: DecoratedBoxTransition(
+                  decoration: _decoration,
+                  child: const SizedBox(
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
