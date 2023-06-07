04de14fa858baa902f924348146f7c83f8459adc
daveg7lee <daveg7lee@gmail.com>
2023-06-07 20:02:10 +0900

    Animation Values

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -10,14 +10,58 @@ class ExplicitAnimationsScreen extends StatefulWidget {
 
 class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController =
-      AnimationController(vsync: this);
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+  );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Explicit Animations"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "${_animationController.value}",
+              style: const TextStyle(
+                fontSize: 58,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _play,
+                  child: const Text("Play"),
+                ),
+                ElevatedButton(
+                  onPressed: _pause,
+                  child: const Text("Pause"),
+                ),
+                ElevatedButton(
+                  onPressed: _rewind,
+                  child: const Text("Rewind"),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
