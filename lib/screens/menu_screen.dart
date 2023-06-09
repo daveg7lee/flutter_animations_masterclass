c6bd421c55d7c961e62cb581cba75e4d7a6ad467
daveg7lee <daveg7lee@gmail.com>
2023-06-09 18:52:49 +0900

    Swiping Gesture

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animations_masterclass/screens/apple_watch_screen.dart';
 import 'package:flutter_animations_masterclass/screens/explicit_animations_screen.dart';
 import 'package:flutter_animations_masterclass/screens/implicits_animations_screen.dart';
+import 'package:flutter_animations_masterclass/screens/swiping_cards_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -51,6 +52,15 @@ class MenuScreen extends StatelessWidget {
                     _goToPage(context, const AppleWatchScreen());
                   },
                   child: const Text("Apple Watch"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _goToPage(context, const SwipingCardsScreen());
+                  },
+                  child: const Text("Swiping Cards"),
                 ),
               )
             ],
