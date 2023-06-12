d313dfb066d8df65bf61b3463d2278ea235163e0
daveg7lee <daveg7lee@gmail.com>
2023-06-12 23:13:19 +0900

    AnimatedSwitcher

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -10,12 +12,43 @@ class MusicPlayerScreen extends StatefulWidget {
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   final PageController _pageController = PageController(viewportFraction: 0.8);
 
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/covers/${_currentPage + 1}.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 15,
+                  sigmaY: 15,
+                ),
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ),
           PageView.builder(
+            onPageChanged: _onPageChanged,
             controller: _pageController,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
@@ -39,15 +72,17 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     'Top Gun',
                     style: TextStyle(
                       fontSize: 26,
+                      color: Colors.white,
                     ),
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 5,
                   ),
                   const Text(
                     "Ki hyun lee",
                     style: TextStyle(
                       fontSize: 18,
+                      color: Colors.white,
                     ),
                   )
                 ],
