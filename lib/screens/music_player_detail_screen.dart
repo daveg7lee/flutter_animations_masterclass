c37d8b800eeb929abe80e605e85e1adf9ca03986
daveg7lee <daveg7lee@gmail.com>
2023-06-13 00:11:03 +0900

    ProgressBar

--- lib/screens/music_player_detail_screen.dart
+++ lib/screens/music_player_detail_screen.dart
@@ -13,6 +13,8 @@ class MusicPlayerDetailScreen extends StatefulWidget {
 class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Top Gun"),
@@ -47,8 +49,59 @@ class _MusicPlayerDetailScreenState extends State<MusicPlayerDetailScreen> {
               ),
             ),
           ),
+          const SizedBox(
+            height: 50,
+          ),
+          CustomPaint(
+            size: Size(size.width - 80, 6),
+            painter: ProgressBar(progressValue: 50),
+          )
         ],
       ),
     );
+  }
+}
+
+class ProgressBar extends CustomPainter {
+  final double progressValue;
+
+  ProgressBar({required this.progressValue});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final trackPaint = Paint()
+      ..color = Colors.grey.shade300
+      ..style = PaintingStyle.fill;
+
+    final trackRRect = RRect.fromLTRBR(
+      0,
+      0,
+      size.width,
+      size.height,
+      const Radius.circular(4),
+    );
+
+    canvas.drawRRect(trackRRect, trackPaint);
+
+    final progressPaint = Paint()
+      ..color = Colors.grey.shade400
+      ..style = PaintingStyle.fill;
+
+    final progressRRect = RRect.fromLTRBR(
+      0,
+      0,
+      progressValue,
+      size.height,
+      const Radius.circular(10),
+    );
+
+    canvas.drawRRect(progressRRect, progressPaint);
+
+    canvas.drawCircle(Offset(progressValue, size.height / 2), 6, progressPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant ProgressBar oldDelegate) {
+    return false;
   }
 }
